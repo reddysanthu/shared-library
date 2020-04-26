5028ef9c2c93738b0fffa1501eb6146b2aa5589c
RoshmiB <42956498+RoshmiB@users.noreply.github.com>
2020-04-26 19:07:12 +0530

    Update config.groovy

--- vars/config.groovy
+++ vars/config.groovy
@@ -1,7 +1,8 @@
 def call(String filePath, Closure closure) {
     def map = readYaml file: filePath
     println map
-    //MavenBuilder builder = new MavenBuilder()
-    //closure.delegate = builder
+    MavenBuilder builder = new MavenBuilder()
+    closure.delegate = builder
+    closure.resolveStrategy = Closure.DELEGATE_FIRST
     closure()
 }
