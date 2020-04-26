193fabcb846eff309e4c1b722afa068c684b3b87
RoshmiB <42956498+RoshmiB@users.noreply.github.com>
2020-04-27 00:47:57 +0530

    Update MavenBuilder.groovy

--- src/MavenBuilder.groovy
+++ src/MavenBuilder.groovy
@@ -2,8 +2,8 @@ class MavenBuilder{
 Map map_var
   def mavenBuild(){
     
-      bat "mvn "+ map_var.builder.maven.goal
-     //"mvn "+ map_var.builder.maven.goal
+      //bat "mvn "+ map_var.builder.maven.goal
+     "mvn "+ map_var.builder.maven.goal
    
   }
 }
