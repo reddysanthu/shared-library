6800e09c4d0c100e07c5fea6fcac7405951447d3
RoshmiB <42956498+RoshmiB@users.noreply.github.com>
2020-05-04 00:41:13 +0530

    Update config.groovy

--- vars/config.groovy
+++ vars/config.groovy
@@ -1,13 +1,16 @@
 def call(String filePath, Closure closure) {
     def request = libraryResource 'globalconfig.yml'
     println "$request"
-    def map1 =  readYaml text: request
-    println map1
-    
-    
-    def map = readYaml file: filePath
-    
+    def map =  readYaml text: request
     println map
+    
+    def access_status = new File(filepath).exists()
+    
+    if (access_status){
+        def map1 = readYaml file: filePath
+        println map1
+        map.putAll(map1)
+    }
 
     MavenBuilder builder = new MavenBuilder(map_var: map)
     closure.delegate = builder
