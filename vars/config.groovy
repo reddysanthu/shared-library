f40a004fea752b3db864ab845424323bd42239ac
RoshmiB <42956498+RoshmiB@users.noreply.github.com>
2020-05-04 00:55:52 +0530

    Update config.groovy

--- vars/config.groovy
+++ vars/config.groovy
@@ -4,18 +4,17 @@ def call(String filePath, Closure closure) {
     def map =  readYaml text: request
     println map
     
-    def var = new File(".").getAbsolutePath()
-    def access_status = new File(var + "/" + filePath).exists()
+    //def var = new File(".").getAbsolutePath()
+    //def access_status = new File(var + "/" + filePath).exists()
     
-    println var
-    //println filepath
-    println access_status
+    //println var
+    //println access_status
     
-    if (access_status){
+   // if (access_status){
         def map1 = readYaml file: filePath
         println map1
         map.putAll(map1)
-    }
+    //}
 
     MavenBuilder builder = new MavenBuilder(map_var: map)
     closure.delegate = builder
