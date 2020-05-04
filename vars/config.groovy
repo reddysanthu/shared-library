1255a3782733c4d5254b794a05f25e89e7161be4
rajasanth <42927089+rajasanth@users.noreply.github.com>
2020-05-04 22:37:12 +0530

    Update config.groovy

--- vars/config.groovy
+++ vars/config.groovy
@@ -5,15 +5,6 @@ def call(String filePath, Closure closure) {
     println map
     
     def file_exist = findFiles (glob: filePath)
-   // println file_exist.length
-    
-    
-    //def var = new File(".").getAbsolutePath()
-    //def access_status = new File(var + "/" + filePath).exists()
-    
-    //println var
-    //println access_status
-    
     if (file_exist.length == 1){
         def map1 = readYaml file: filePath
         println map1
