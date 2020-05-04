07e1131742ce6ecfd9b969bfa8d8e766daff2d74
rajasanth <42927089+rajasanth@users.noreply.github.com>
2020-05-04 22:46:56 +0530

    Update config.groovy

--- vars/config.groovy
+++ vars/config.groovy
@@ -4,16 +4,23 @@ def call(String filePath, Closure closure) {
     def map =  readYaml text: request
     println map
     
-    def file_exist = findFiles (glob: filePath)
-    if (file_exist.length == 1){
-        def map1 = readYaml file: filePath
-        println map1
-        map.putAll(map1)
+    if (!filePath.isempty) {
+            def file_exist = findFiles (glob: filePath)
+            if (file_exist.length == 1) {
+                def map1 = readYaml file: filePath
+                println map1
+                map.putAll(map1)
+            }
     }
+
 
     MavenBuilder builder = new MavenBuilder(map_var: map)
     closure.delegate = builder
     closure.resolveStrategy = Closure.DELEGATE_FIRST
     closure()
+}
+
+def call(Closure closure) {
+    call('', closure)
 }
 
